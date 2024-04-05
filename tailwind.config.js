9b9732cc2cde2f0c14eabbba5ab5330a57c95781
Denis-Turbatu <turbatudenis81@gmail.com>
2024-04-05 09:45:54 +0200

    add: project init

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html}"],
+  content: ["./dist/js/*.{js}"],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};

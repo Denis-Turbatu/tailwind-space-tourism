8ebfb4f572398d0253982c7e49ba114867de21fe
Denis-Turbatu <turbatudenis81@gmail.com>
2024-04-09 18:08:07 +0200

    fix: chang path images

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.{html}"],
-  content: ["./dist/js/*.{js}"],
+  content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {}
+    extend: {
+      backgroundImage: {
+        // HOME
+        "home-mobile":
+          "url('../image/background-home-mobile.jpg')",
+        "home-tablet":
+          "url('../image/background-home-tablet.jpg')",
+        "home-desktop":
+          "url('../image/background-home-desktop.jpg')",
+        // DESTINATION
+        "destination-mobile":
+          "url('../image/background-destination-mobile.jpg')",
+        "destination-tablet":
+          "url('../image/background-destination-tablet.jpg')",
+        "destination-desktop":
+          "url('../image/background-destination-desktop.jpg')",
+        // CREW
+        "crew-mobile":
+          "url('../image/background-crew-mobile.jpg')",
+        "crew-tablet":
+          "url('../image/background-crew-tablet.jpg')",
+        "crew-desktop":
+          "url('../image/background-crew-desktop.jpg')"
+      }
+    }
   },
   plugins: []
 };
